ee1e7c9e6ad628b3e9967ad633e7fc40a54c34fd
Andrew Monks <a@monks.co>
2013-05-16 22:51:56 -0400

    big changes

--- lilb.rb
+++ lilb.rb
@@ -1,16 +1,16 @@
-# # lilb.rb
-# require 'sinatra'
-# require 'maruku'
-# require 'execjs'
-# require 'haml'
-# require 'sass'
+# lilb.rb
+
+require 'sinatra'
+require 'maruku'
+require 'execjs'
+require 'haml'
 
 require 'htmlentities'
 require 'nokogiri'
 require 'open-uri'
 require 'net/http'
 
-
+# to check if lyricswiki has the lyrics to a particular song
 def remote_file_exists?(url)
   url = URI.parse(url)
   Net::HTTP.start(url.host, url.port) do |http|
@@ -18,36 +18,42 @@ def remote_file_exists?(url)
   end
 end
 
-
+# lyricswiki encodes lyrics as individual html entities. get ready to parse!
 coder = HTMLEntities.new
 
-
+# open Lil B songs list; make array of song urls
+# worth doing this every load in case new lyrics have been added
 doc = Nokogiri::HTML(open("http://lyrics.wikia.com/api.php?func=getSong&artist=Lil_B&fmt=html"))
 songs = Array.new
 doc.xpath('//li/ul/li/a').map  { |link| link['href'] }.each do |href|
+	# only add url to array if lyricswiki has the lyrics
 	if remote_file_exists?(href)
 		songs.push(href)
 		puts "added " + href
 	end
 end
 
+# string to hold all lyrics for markoving
 lyrics = String.new
+# open each song
 songs.each do |song|
-	print song
 	doc = Nokogiri::HTML(open(song))
-	lyrics = lyrics + "\n " + coder.decode(doc.xpath('//div[@class="lyricbox"]').xpath('text()').to_s)
+	# get lyricsbox
+	lyricsbox = doc.xpath('//div[@class="lyricbox"]')
+	# replace br tags with periods so we can safely strip extra lyricswiki tags
+	lyricsbox.css('br').each{ |br| br.replace ". " }
+	# strip extra tags, add newlines after punctuation (for readability i guess), decode entities, and add to lyrics string
+	lyrics = lyrics + coder.decode(lyricsbox.xpath('text()').to_s.gsub(/[\.\!\?]/,". \n"))
 end
 
 
-print lyrics
 
+get '/' do
+	@lyrics = lyrics
+	haml :generate
+end
 
-# get '/' do
-# 	@lyrics = lyrics
-# 	haml :generate
-# end
-
-# get '/stylesheets/style.css' do
-#   sass :style
-# end
+get '/stylesheets/style.css' do
+  sass :style
+end
 
