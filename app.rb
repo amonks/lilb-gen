d93cf35d143b90a56db317cec7165f1aba8ed5b3
Andrew Monks <a@monks.co>
2013-05-16 23:07:15 -0400

    before

--- app.rb
+++ app.rb
@@ -22,31 +22,33 @@ end
 # lyricswiki encodes lyrics as individual html entities. get ready to parse!
 coder = HTMLEntities.new
 
-# open Lil B songs list; make array of song urls
-# worth doing this every load in case new lyrics have been added
-doc = Nokogiri::HTML(open("http://lyrics.wikia.com/api.php?func=getSong&artist=Lil_B&fmt=html"))
-songs = Array.new
-doc.xpath('//li/ul/li/a').map  { |link| link['href'] }.each do |href|
-	# only add url to array if lyricswiki has the lyrics
-	if remote_file_exists?(href)
-		songs.push(href)
-		puts "added " + href
+# lolololol every request is gonna take like 3 minutes this is a problem
+before do
+	# open Lil B songs list; make array of song urls
+	# worth doing this every load in case new lyrics have been added
+	doc = Nokogiri::HTML(open("http://lyrics.wikia.com/api.php?func=getSong&artist=Lil_B&fmt=html"))
+	songs = Array.new
+	doc.xpath('//li/ul/li/a').map  { |link| link['href'] }.each do |href|
+		# only add url to array if lyricswiki has the lyrics
+		if remote_file_exists?(href)
+			songs.push(href)
+			puts "added " + href
+		end
+	end
+
+	# string to hold all lyrics for markoving
+	lyrics = String.new
+	# open each song
+	songs.each do |song|
+		doc = Nokogiri::HTML(open(song))
+		# get lyricsbox
+		lyricsbox = doc.xpath('//div[@class="lyricbox"]')
+		# replace br tags with periods so we can safely strip extra lyricswiki tags
+		lyricsbox.css('br').each{ |br| br.replace ". " }
+		# strip extra tags, add newlines after punctuation (for readability i guess), decode entities, and add to lyrics string
+		lyrics = lyrics + coder.decode(lyricsbox.xpath('text()').to_s.gsub(/[\.\!\?]/,". \n"))
 	end
 end
-
-# string to hold all lyrics for markoving
-lyrics = String.new
-# open each song
-songs.each do |song|
-	doc = Nokogiri::HTML(open(song))
-	# get lyricsbox
-	lyricsbox = doc.xpath('//div[@class="lyricbox"]')
-	# replace br tags with periods so we can safely strip extra lyricswiki tags
-	lyricsbox.css('br').each{ |br| br.replace ". " }
-	# strip extra tags, add newlines after punctuation (for readability i guess), decode entities, and add to lyrics string
-	lyrics = lyrics + coder.decode(lyricsbox.xpath('text()').to_s.gsub(/[\.\!\?]/,". \n"))
-end
-
 
 
 get '/' do
