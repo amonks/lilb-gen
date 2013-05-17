05230c3a40b21a8478b0e7b3d98a0928550a941a
Andrew Monks <a@monks.co>
2013-05-17 00:10:22 -0400

    spider

--- public/GibGen.js
+++ public/GibGen.js
@@ -2,89 +2,88 @@
 // Algorithm: Letter-based Markov text generator.
 // based on work by Keith Enevoldsen, thinkzone.wlonk.com
 
-function generate_gibberish(sample,lev)
-{
-// Clear output.
-outtext = ""
+function generate_gibberish(sample,lev) {
+   // Clear output.
+   outtext = ""
 
-// Make the string contain two copies of the input text.
-// This allows for wrapping to the beginning when the end is reached.
-str = sample + " "
-nchars = str.length
-str = str + str
+   // Make the string contain two copies of the input text.
+   // This allows for wrapping to the beginning when the end is reached.
+   str = sample + " "
+   nchars = str.length
+   str = str + str
 
-// Get level.
-// for (i = 0; i < form.level.length; i++) {
-//    if (form.level[i].checked) {
-//       lev = parseInt(form.level[i].value)
-//    }
-// }
+   // Get level.
+   // for (i = 0; i < form.level.length; i++) {
+   //    if (form.level[i].checked) {
+   //       lev = parseInt(form.level[i].value)
+   //    }
+   // }
 
 
-// Check input length.
-if (nchars < lev) {
-//   alert("Too few input characters.")
-   return
-}
+   // Check input length.
+   if (nchars < lev) {
+   //   alert("Too few input characters.")
+      return
+   }
 
-// Pick a random starting character, preferably an uppercase letter.
-for (i = 0; i < 1000; i++) {
-   ichar = Math.floor(nchars * Math.random())
-   chr = str.charAt(ichar)
-   if ((chr >= "A") && (chr <= "Z")) break
-}
+   // Pick a random starting character, preferably an uppercase letter.
+   for (i = 0; i < 1000; i++) {
+      ichar = Math.floor(nchars * Math.random())
+      chr = str.charAt(ichar)
+      if ((chr >= "A") && (chr <= "Z")) break
+   }
 
-// Write starting characters.
-outtext = outtext + str.substring(ichar, ichar + lev)
+   // Write starting characters.
+   outtext = outtext + str.substring(ichar, ichar + lev)
 
-// Set target string.
-target = str.substring(ichar + 1, ichar + lev)
+   // Set target string.
+   target = str.substring(ichar + 1, ichar + lev)
 
-// Generate characters.
-// Algorithm: Letter-based Markov text generator.
-for (i = 0; i < 500; i++) {
-   if (lev == 1) {
-      // Pick a random character.
-      chr = str.charAt(Math.floor(nchars * Math.random()))
-   } else {
-      // Find all sets of matching target characters.
-      nmatches = 0
-      j = -1
-      while (true) {
-         j = str.indexOf(target, j + 1)
-         if ((j < 0) || (j >= nchars)) {
-            break
-         } else {
-            nmatches++
+   // Generate characters.
+   // Algorithm: Letter-based Markov text generator.
+   for (i = 0; i < 500; i++) {
+      if (lev == 1) {
+         // Pick a random character.
+         chr = str.charAt(Math.floor(nchars * Math.random()))
+      } else {
+         // Find all sets of matching target characters.
+         nmatches = 0
+         j = -1
+         while (true) {
+            j = str.indexOf(target, j + 1)
+            if ((j < 0) || (j >= nchars)) {
+               break
+            } else {
+               nmatches++
+            }
+         }
+
+         // Pick a match at random.
+         imatch = Math.floor(nmatches * Math.random())
+
+         // Find the character following the matching characters.
+         nmatches = 0
+         j = -1
+         while (true) {
+            j = str.indexOf(target, j + 1)
+            if ((j < 0) || (j >= nchars)) {
+               break
+            } else if (imatch == nmatches) {
+               chr = str.charAt(j + lev - 1)
+               break
+            } else {
+               nmatches++
+            }
          }
       }
 
-      // Pick a match at random.
-      imatch = Math.floor(nmatches * Math.random())
+      // Output the character.
+      outtext = outtext + chr
 
-      // Find the character following the matching characters.
-      nmatches = 0
-      j = -1
-      while (true) {
-         j = str.indexOf(target, j + 1)
-         if ((j < 0) || (j >= nchars)) {
-            break
-         } else if (imatch == nmatches) {
-            chr = str.charAt(j + lev - 1)
-            break
-         } else {
-            nmatches++
-         }
+      // Update the target.
+      if (lev > 1) {
+         target = target.substring(1, lev - 1) + chr
       }
    }
-
-   // Output the character.
-   outtext = outtext + chr
-
-   // Update the target.
-   if (lev > 1) {
-      target = target.substring(1, lev - 1) + chr
-   }
-}
-return outtext
+   return outtext
 }
--- spider.rb
+++ spider.rb
@@ -7,6 +7,7 @@ require 'net/http'
 
 
 
+
 # to check if lyricswiki has the lyrics to a particular song
 def remote_file_exists?(url)
   url = URI.parse(url)
@@ -15,34 +16,43 @@ def remote_file_exists?(url)
   end
 end
 
-# lyricswiki encodes lyrics as individual html entities. get ready to parse!
-coder = HTMLEntities.new
+def get_all_lyrics_by(artist)
 
-# until I figure out where to put this so it doesn't timeout i'm just gonna run with lyrics in a .txt file
+	# lyricswiki encodes lyrics as individual html entities. get ready to parse!
+	coder = HTMLEntities.new
 
-# open Lil B songs list; make array of song urls
-# worth doing this every load in case new lyrics have been added
-doc = Nokogiri::HTML(open("http://lyrics.wikia.com/api.php?func=getSong&artist=Lil_B&fmt=html"))
-songs = Array.new
-doc.xpath('//li/ul/li/a').map  { |link| link['href'] }.each do |href|
-	# only add url to array if lyricswiki has the lyrics
-	if remote_file_exists?(href)
-		songs.push(href)
-		puts "added " + href
+	# until I figure out where to put this so it doesn't timeout i'm just gonna run with lyrics in a .txt file
+
+	# open songs list; make array of song urls
+	# worth doing this every load in case new lyrics have been added
+	doc = Nokogiri::HTML(open("http://lyrics.wikia.com/api.php?func=getSong&artist=" + artist + "&fmt=html"))
+	songs = Array.new
+	doc.xpath('//li/ul/li/a').map  { |link| link['href'] }.each do |href|
+		# only add url to array if lyricswiki has the lyrics
+		if remote_file_exists?(href)
+			songs.push(href)
+			puts "added " + href
+		end
 	end
+
+	# string to hold all lyrics
+	lyrics = String.new
+	# open each song
+	songs.each do |song|
+		doc = Nokogiri::HTML(open(song))
+		# get lyricsbox
+		lyricsbox = doc.xpath('//div[@class="lyricbox"]')
+		# replace br tags with periods so we can safely strip extra lyricswiki tags
+		lyricsbox.css('br').each{ |br| br.replace ". " }
+		# strip extra tags, add newlines after punctuation (for readability i guess), decode entities, and add to lyrics string
+		lyrics = lyrics + coder.decode(lyricsbox.xpath('text()').to_s.gsub(/[\.\!\?]/,". \n"))
+	end
+
+	return lyrics
 end
 
-# string to hold all lyrics for markoving
-lyrics = String.new
-# open each song
-songs.each do |song|
-	doc = Nokogiri::HTML(open(song))
-	# get lyricsbox
-	lyricsbox = doc.xpath('//div[@class="lyricbox"]')
-	# replace br tags with periods so we can safely strip extra lyricswiki tags
-	lyricsbox.css('br').each{ |br| br.replace ". " }
-	# strip extra tags, add newlines after punctuation (for readability i guess), decode entities, and add to lyrics string
-	lyrics = lyrics + coder.decode(lyricsbox.xpath('text()').to_s.gsub(/[\.\!\?]/,". \n"))
-end
+# get all lil B lyrics
+output = get_all_lyrics_by("Lil_B")
 
-File.open("public/lyrics.txt", 'w') {|f| f.write(lyrics) }
+# and write them to a .txt file
+File.open("public/lyrics.txt", 'w') {|f| f.write(output.encode('ascii', :invalid => :replace, :undef => :replace, :replace => '_')) }
